fae3a6f35591c3bf831f5913b13fd44f2f716a61
Ashkan Jangodaz <ajangoda@nyit.edu>
2022-03-10 01:31:11 -0800

    Update Euler3.py

--- python solutions/Euler3.py
+++ python solutions/Euler3.py
@@ -1,24 +1,28 @@
 def is_prime(n):
 
-    flag = False
-    if n > 1:
-        for i in range(2, n):
-            if (n % i) == 0:
-                flag = True
-                break
-        return flag
+    if n == 2:
+        return True
+    if n > 2:
+        for i in range (2, n):
+            a = n % i
+            if a == 0:
+                return False
+        else:
+            return True
 
 def prime_factors_finder(n):
+
     prime_factors = []
     if n > 1:
         for i in range(2, n):
             if (n % i) == 0:
                 if is_prime(i):
-                    break
-                prime_factors.append(i)
+                    prime_factors.append(i)
+
         print("Prime factors list:" , prime_factors)
         print("Largest prime factor is:" , max(prime_factors)) 
 
 a = int(input("enter your desired number: "))
 print(prime_factors_finder(a))
+
 
