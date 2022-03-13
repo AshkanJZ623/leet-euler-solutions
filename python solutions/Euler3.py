1fcfb8509be4bb9e07a3714b8300d27be3a8ab7e
Ashkan Jangodaz <ajangoda@nyit.edu>
2022-03-13 05:19:14 -0700

    Update Euler3.py

--- python solutions/Euler3.py
+++ python solutions/Euler3.py
@@ -1,4 +1,7 @@
 import math
+import time
+
+
 
 def is_prime(n):
 
@@ -20,13 +23,18 @@ def prime_factors_finder(n):
             prime_factors.append(n)
             print("Your number" , n, "is prime") 
         else:
-            for i in range(2, int(math.sqrt(n)+1)):
+            for i in range(2,int(math.sqrt(n)+1)):
                 if (n % i) == 0:
                     if is_prime(i):
                         prime_factors.append(i)
+                        # n //= i
             print("Prime factors list:" , prime_factors)
             print("Largest prime factor is:" , max(prime_factors))  
         
+# start_time = time.time()
+
 
 a = int(input("enter your desired number: "))
 print(prime_factors_finder(a))
+
+# print(time.time() - start_time, "seconds")
